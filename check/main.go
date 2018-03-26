0cdf6a0c21862b6409ae736e23bf28fb2e1f5a09
Wenhui Shen <swhbox@foxmail.com>
2018-03-26 14:20:00 +0800

    update

--- check/main.go
+++ check/main.go
@@ -33,6 +33,8 @@ func checkLicense(verbose bool) error {
 
 	if verbose {
 		fmt.Println("Name:", license.Info.Name)
+		fmt.Println("Machine ID:", license.Info.MachineID)
+		fmt.Println("License ID:", license.Info.LicenseID)
 		fmt.Println("Expiry:", license.Info.Expiration)
 		fmt.Println("Key:", license.Key)
 	}
