5c13065e61018775329d33ff8d427deef1392289
Wenhui Shen <swhbox@foxmail.com>
2018-04-28 12:17:28 +0800

    update

--- lib/licensing.go
+++ lib/licensing.go
@@ -44,25 +44,19 @@ type LicenseInfo struct {
 	Version    string    `json:"version,omitempty"`
 	Expiration time.Time `json:"expiration"`
 	Extra      Validator `json:"extra,omitempty"`
-	durafmt    *com.Durafmt
 }
 
-func (a *LicenseInfo) Remaining(langs ...string) *com.Durafmt {
+func (a LicenseInfo) Remaining(langs ...string) *com.Durafmt {
 	if a.Expiration.IsZero() {
 		return nil
-	}
-	if a.durafmt != nil {
-		return a.durafmt
 	}
 	now := time.Now()
 	duration := a.Expiration.Sub(now)
 	//duration *= -1
 	if len(langs) > 0 {
-		a.durafmt = com.ParseDuration(duration, langs[0])
-	} else {
-		a.durafmt = com.ParseDuration(duration)
+		return com.ParseDuration(duration, langs[0])
 	}
-	return a.durafmt
+	return com.ParseDuration(duration)
 }
 
 // LicenseData - This is the license data we serialise into a license file
